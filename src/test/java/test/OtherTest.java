5ff67d731db98819605ba3d28c8e54cc52f214c3
Hadeel Samya <h.samya@alpha-apps.ae>
2022-04-09 15:56:36 +0300

    add coverage

--- src/test/java/test/OtherTest.java
+++ src/test/java/test/OtherTest.java
@@ -25,11 +25,57 @@
 package test;
 
 import org.junit.Test;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Assert;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.RestTemplate;
 
 public class OtherTest extends Base {
 
-    @Test public void mytest() {
-        run();
+    @Test public void mytest() throws Exception {
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>();
+        map.add("email", "CMS@CMS.com");
+        map.add("password", "Admin123");
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        final HttpEntity<MultiValueMap<String, String>> entity = new HttpEntity<MultiValueMap<String, String>>(map,
+                headers);
+        JSONObject jsonObject = null;
+
+        try {
+            RestTemplate restTemplate = new RestTemplate();
+            ResponseEntity<String> responseEntity = restTemplate.exchange(
+                    "http://localhost:3000/auth/signin", HttpMethod.POST, entity,
+                    String.class);
+
+            if (responseEntity.getStatusCode() == HttpStatus.CREATED) {
+                try {
+                    jsonObject = new JSONObject(responseEntity.getBody());
+                    Assert.assertEquals(201, responseEntity.getStatusCodeValue());
+                    System.out.print(jsonObject);
+                } catch (JSONException e) {
+                    throw new RuntimeException("JSONException occurred");
+                }
+            }
+        } catch (final HttpClientErrorException httpClientErrorException) {
+            throw new Exception();
+        } catch (HttpServerErrorException httpServerErrorException) {
+            throw new Exception(httpServerErrorException);
+        } catch (Exception exception) {
+            throw new Exception(exception);
+        }
     }
 
 }
